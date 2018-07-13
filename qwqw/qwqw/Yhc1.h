a854ecde643f29aead857b9a70ff812955d4f494
1548397772 <xsb537@163.com>
2018-07-13 10:30:38 +0800

    add yhc1 file

---
+++ qwqw/qwqw/Yhc1.h
@@ -0,0 +1,13 @@
+//
+//  Yhc1.h
+//  qwqw
+//
+//  Created by BJyhc on 2018/7/13.
+//  Copyright © 2018年 blue@circle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yhc1 : NSObject
+
+@end
---
+++ qwqw/qwqw/Yhc1.m
@@ -0,0 +1,13 @@
+//
+//  Yhc1.m
+//  qwqw
+//
+//  Created by BJyhc on 2018/7/13.
+//  Copyright © 2018年 blue@circle. All rights reserved.
+//
+
+#import "Yhc1.h"
+
+@implementation Yhc1
+
+@end
