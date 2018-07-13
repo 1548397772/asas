3835c10fc344971f47dadc7accfd94126ca68db7
1548397772 <xsb537@163.com>
2018-07-13 10:31:08 +0800

    add yhc file

---
+++ qwqw/qwqw/Yhc.h
@@ -0,0 +1,13 @@
+//
+//  Yhc.h
+//  qwqw
+//
+//  Created by BJyhc on 2018/7/13.
+//  Copyright © 2018年 blue@circle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Yhc : NSObject
+
+@end
---
+++ qwqw/qwqw/Yhc.m
@@ -0,0 +1,13 @@
+//
+//  Yhc.m
+//  qwqw
+//
+//  Created by BJyhc on 2018/7/13.
+//  Copyright © 2018年 blue@circle. All rights reserved.
+//
+
+#import "Yhc.h"
+
+@implementation Yhc
+
+@end
